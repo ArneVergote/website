47c669ff1b94a4e0d9084483a5ea1bf48d5ce54d
ArneVergote <arne.vergote2@student.vives.be>
2024-03-02 18:02:13 +0100

    d

--- Lab_3/Colorpicker/scripts/code.js
+++ Lab_3/Colorpicker/scripts/code.js
@@ -2,17 +2,12 @@ const setup = () => {
 
 	let sliders = document.getElementsByClassName("slider");
 
-	// we moeten zowel op het input als het change event reageren,
-	// zie http://stackoverflow.com/questions/18544890
 	sliders[0].addEventListener("change", update);
 	sliders[0].addEventListener("input", update);
 	sliders[1].addEventListener("change", update);
 	sliders[1].addEventListener("input", update);
 	sliders[2].addEventListener("change", update);
 	sliders[2].addEventListener("input", update);
-
-	// maak het blokje rood
-
 }
 
 	const update = () => {
