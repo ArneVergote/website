93c2c1667c314622c9d3dd50323ed5976c73d787
ArneVergote <arne.vergote2@student.vives.be>
2024-03-13 19:37:20 +0100

    d

---
+++ Lab_5/Opdracht formwaarden/scripts/code.js
@@ -0,0 +1,50 @@
+const setup = () => {
+    document.getElementById('button').addEventListener('click', toonResultaat);
+};
+
+function toonResultaat() {
+    const form = document.getElementById('myForm');
+    const formData = new FormData(form);
+    const data = {};
+
+    for (const [key, value] of formData.entries()) {
+        data[key] = value;
+    }
+
+    let result = "";
+    if (data['isRoker'] === undefined) {
+        result += "Er is geen roker.\n";
+    } else {
+        result += "Er is een roker.\n";
+    }
+
+    if (data['moedertaal'] === 'nl') {
+        result += "Moedertaal is Nederlands.\n";
+    } else if (data['moedertaal'] === 'fr') {
+        result += "Moedertaal is Frans.\n";
+    } else if (data['moedertaal'] === 'en') {
+        result += "Moedertaal is Engels.\n";
+    }
+
+    result += "Favoriete buurland is " + data['favorieteBuurland'] + ".\n";
+
+    const bestellingen = data['bestelling'];
+    result += "Bestelling bestaat uit ";
+    if (Array.isArray(bestellingen)) {
+        if (bestellingen.length > 0) {
+            result += bestellingen.join(", ") + ".";
+        } else {
+            result += "geen bestelling.";
+        }
+    } else if (bestellingen !== undefined) {
+        result += bestellingen + ".";
+    } else {
+        result += "geen bestelling.";
+    }
+
+    console.log(result);
+
+}
+
+window.addEventListener("load", setup);
+
