8e0eb9a32368bdea73bbb78e6502eca1befe4fe8
ArneVergote <arne.vergote2@student.vives.be>
2024-04-24 11:31:53 +0200

    d

--- Lab_9/Start_Colorpicker/scripts/swatch.js
+++ Lab_9/Start_Colorpicker/scripts/swatch.js
@@ -16,21 +16,17 @@ const configureSwatch = (swatch, red, green, blue) => {
 
 const buildSwatchComponent = (red, green, blue) => {
 
-        // maak de twee element nodes
         let swatch = document.createElement("div");
         let btnDelete = document.createElement("input");
 
-        // stel de swatch in
         swatch.className = "swatch";
         configureSwatch(swatch, red, green, blue);
         swatch.addEventListener("click", setColorPickerFromSwatch);
 
-        // stel de delete knop in
         btnDelete.setAttribute("type", "button");
         btnDelete.setAttribute("value", "X");
         btnDelete.addEventListener("click", deleteSwatch);
-
-        // voeg de swatch en button toe aan de swatchcomponent
+        
         swatch.appendChild(btnDelete);
         return swatch;
     };
